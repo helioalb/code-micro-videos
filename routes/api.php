0967149cbb9bf7d32604f3442536031817cf9fb5
Helio Albano de Oliveira <helioalb@gmail.com>
2021-07-15 12:17:01 +0000

    Desafio pratico - Criando recurso Category
    Nesta primeira fase de projeto, crie o recurso de Category como descrito nas aulas:
    
    Model
    Factory e seeder
    Controller
    E crie mais um recurso, que chamaremos de Gênero.
    Lembrando que o Gênero no sistema significa: Terror, ação, comédia e etc. Ele será relacionado com o Vídeo mais tarde.
    
    O Gênero deverá ter os seguintes campos: nome, is_active (Se está ativo ou não).
    
    Se divirta com a fase e ao final envie o projeto para correção como de costume.
    
    Boa sorte!
    
    Comandos usados:
    
    php artisan make:model Models/Genre --all
    
    php artisan migrate:fresh --seed
    
    php artisan make:seeder GenresTableSeeder
    
    php artisan migrate:refresh --seed

--- routes/api.php
+++ routes/api.php
@@ -21,4 +21,8 @@ Route::group(['namespace' => 'Api'], function() {
     Route::resource('categories', 
                     'CategoryController', 
                     ['except' => ['create', 'edit']]);
+
+    Route::resource('genres',
+                    'GenreController',
+                    ['except' => ['create', 'edit']]);
 });
